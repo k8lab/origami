e0f6e7c5a56e6cee4811e64a1e657ee0c8e02fd3
k8lab <kimito.skyline@gamil.com>
2024-08-08 14:42:50 +0000

    build package

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/sample.ts'),
-      name: 'index',
-      fileName: 'index',
+      name: 'sample',
+      fileName: 'sample',
+      formats: ['es', 'umd']
     },
   },  
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      exclude: ['src/main.ts']
+    })
+  ],
 })
